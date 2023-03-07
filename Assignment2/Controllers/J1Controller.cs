8858fec3474b31460c3bea2175f6cd9200f1dcd7
ali <alibhamze@hotmail.com>
2023-03-06 16:58:56 -0800

    Changes 2

--- Assignment2/Controllers/J1Controller.cs
+++ Assignment2/Controllers/J1Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Principal;
 using System.Web.Http;
 
 namespace Assignment2.Controllers
@@ -19,4 +20,63 @@ namespace Assignment2.Controllers
 
         return $"Your total calorie count is {totalCalories}";
     }
+    private int GetBurgerCalories(int burger)
+    {
+        if (burger == 1)
+            return 461;
+        else if (burger == 2)
+            return 431;
+        else if (burger == 3)
+            return 420;
+        else
+            return 0;
+
+    }
+
+    private int GetDrinkCalories(int drink)
+    {
+        if (drink == 1)
+            return 130;
+
+        else if (drink == 2)
+            return 160;
+        else if (drink == 3)
+            return 118;
+        else
+            return 0;
+    }
+
+    private int GetSideCalories(int side)
+    {
+
+        if (side = 1) 
+
+            return 100;  
+        else if (side == 2) 
+            return 57; 
+        else if (side == 3) 
+            return 70; 
+
+        else
+            return 0
+    }
+
+    private int GetDessertCalories(int dessert)
+    {
+
+        if (dessert = 1) //Apple Pie
+
+            return 167; 
+        else if (dessert == 2)
+            return 266; 
+
+        else if (dessert = 3) 
+
+            return 75; 
+        return 0;
+    }
+
+   
+  
 }
+
--- Assignment2/Controllers/j2Controller.cs
+++ Assignment2/Controllers/j2Controller.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Web.Http;
 
 namespace Assignment2.Controllers
 {
-    public class j2Controller : ApiController
-    {
+    public string Get(int m, int n) {
+
+        int numOfWays = GetNumOfWays(m, n, 10);
+
+        if (numOfWays == 1)
+
+            return $"There is 1 ways to get the sum 10";
+
+return $"There are {numOfWays} ways to get the sum 10";
     }
-}
+
+    private int GetNumOfways(int dice1sides, int dice2Sides, int targetValue)
+    {
+        int waysToRoll = 0;
+
+        for (int i = 1; i <= dice1sides; i++)
+        {
+
+
+
+            for (int j = 1; j <= dice2Sides; j++)
+
+            {
+
+                if (i + j = targetValue)
+                {
+                    waysToRoll++;
+                }
+
+
+            }
+        }
+
+        return waysToRoll;
+
+    }
